33319cbbc413950d612eef5d9cf13c0c81424d5e
jorgea-hn <jorgeahenriqueznovoa@gmail.com>
2023-04-14 13:15:03 -0500

    Focus en el text area y condicion para cuando no se ha escrito

--- index.js
+++ index.js
@@ -27,112 +27,120 @@ var botonc = document.querySelector("#boton-copiar")
 var botone = document.querySelector("#boton-encriptar")
 var botond = document.querySelector("#boton-desencriptar")
 
-
+texto.focus()
 botone.addEventListener('click', () => {
-    var mayuscula = probarMayusculas(texto.value)
+  var mayuscula = probarMayusculas(texto.value)
 
-    if (mayuscula==1){
-        alert("Favor ingresar texto en minuscula")
-    }else{
-        vacio.style.display="none"
-        textareaDes.value = cambiarVocales(texto.value)
-        textareaDes.style.display="block"  
-        texto.value = ""
-        copiar.style.display="block"
+  if (mayuscula == 1) {
+    alert("Favor ingresar texto en minuscula")
+  } else {
+    if (texto.value == "") {
+      alert("Texto vacio")
+    } else {
+      vacio.style.display = "none"
+      textareaDes.value = cambiarVocales(texto.value)
+      textareaDes.style.display = "block"
+      texto.value = ""
+      copiar.style.display = "block"
     }
-    
+  }
+
 });
 
 botond.addEventListener('click', () => {
 
-    var mayuscula = probarMayusculas(texto.value)
-    
-    if (mayuscula==1){
-        alert("Favor ingresar texto en minuscula")
-    }else{
-        vacio.style.display="none"
-        textareaDes.value = revertirCambioDeVocales(texto.value)
-        textareaDes.style.display="block"
-        texto.value = ""
-        copiar.style.display="block"
+  var mayuscula = probarMayusculas(texto.value)
+
+  if (mayuscula == 1) {
+    alert("Favor ingresar texto en minuscula")
+  } else {
+    if (texto.value == "") {
+      alert("Texto vacio")
+    } else {
+    vacio.style.display = "none"
+    textareaDes.value = revertirCambioDeVocales(texto.value)
+    textareaDes.style.display = "block"
+    texto.value = ""
+    copiar.style.display = "block"
     }
-    
+  }
+
 });
 
 
 botonc.addEventListener('click', () => {
-    navigator.permissions.query({name: "clipboard-write"}).then((result) => {
-        if (result.state === "granted" || result.state === "prompt") {
-          /* write to the clipboard now */
-        }
-    });
-    textareaDes.select()
-    document.execCommand("copy")
+  navigator.permissions.query({ name: "clipboard-write" }).then((result) => {
+    if (result.state === "granted" || result.state === "prompt") {
+      /* write to the clipboard now */
+    }
+  });
+  textareaDes.select()
+  document.execCommand("copy")
 });
 
 
 
-function probarMayusculas(texto){
-    for (var i = 0; i < texto.length; i++){
-        if (texto[i].toUpperCase()===texto[i]){
-            return 1
-        }
-        else{
-            return 0
-        }
+function probarMayusculas(texto) {
+  for (var i = 0; i < texto.length; i++) {
+    if (texto[i].toUpperCase() === texto[i]) {
+      return 1
     }
+    else {
+      return 0
+    }
+  }
 }
 
 function cambiarVocales(texto) {
-    let fraseModificada = "";
-    for (var i = 0; i < texto.length; i++) {
-      if (texto[i] == "a") {
-        fraseModificada += "ai";
-      } else if (texto[i] == "e") {
-        fraseModificada += "enter";
-      } else if (texto[i] == "i") {
-        fraseModificada += "imes";
-      } else if (texto[i] == "o") {
-        fraseModificada += "ober";
-      } else if (texto[i] == "u") {
-        fraseModificada += "ufat";
-      } else {
-        fraseModificada += texto[i];
-      }
+  let fraseModificada = "";
+  for (var i = 0; i < texto.length; i++) {
+    if (texto[i] == "a") {
+      fraseModificada += "ai";
+    } else if (texto[i] == "e") {
+      fraseModificada += "enter";
+    } else if (texto[i] == "i") {
+      fraseModificada += "imes";
+    } else if (texto[i] == "o") {
+      fraseModificada += "ober";
+    } else if (texto[i] == "u") {
+      fraseModificada += "ufat";
+    } else {
+      fraseModificada += texto[i];
     }
-    return fraseModificada;
   }
+  return fraseModificada;
+}
 
 
 
-  function revertirCambioDeVocales(fraseModificada) {
-    let textoOriginal = "";
-    for (var i = 0; i < fraseModificada.length; i++) {
-      if (fraseModificada.slice(i, i + 2) == "ai") {
-        textoOriginal += "a";
-        i++; // Saltamos el siguiente índice, ya que ya fue reemplazado
-      } else if (fraseModificada.slice(i, i + 5) == "enter") {
-        textoOriginal += "e";
-        i += 4; // Saltamos los siguientes cuatro índices, ya que ya fueron reemplazados
-      } else if (fraseModificada.slice(i, i + 4) == "imes") {
-        textoOriginal += "i";
-        i += 3; // Saltamos los siguientes tres índices, ya que ya fueron reemplazados
-      } else if (fraseModificada.slice(i, i + 4) == "ober") {
-        textoOriginal += "o";
-        i += 3; // Saltamos los siguientes tres índices, ya que ya fueron reemplazados
-      } else if (fraseModificada.slice(i, i + 4) == "ufat") {
-        textoOriginal += "u";
-        i += 3; // Saltamos los siguientes tres índices, ya que ya fueron reemplazados
-      } else {
-        textoOriginal += fraseModificada[i];
-      }
+function revertirCambioDeVocales(fraseModificada) {
+  let textoOriginal = "";
+  for (var i = 0; i < fraseModificada.length; i++) {
+    if (fraseModificada.slice(i, i + 2) == "ai") {
+      textoOriginal += "a";
+      i++; // Saltamos el siguiente índice, ya que ya fue reemplazado
+    } else if (fraseModificada.slice(i, i + 5) == "enter") {
+      textoOriginal += "e";
+      i += 4; // Saltamos los siguientes cuatro índices, ya que ya fueron reemplazados
+    } else if (fraseModificada.slice(i, i + 4) == "imes") {
+      textoOriginal += "i";
+      i += 3; // Saltamos los siguientes tres índices, ya que ya fueron reemplazados
+    } else if (fraseModificada.slice(i, i + 4) == "ober") {
+      textoOriginal += "o";
+      i += 3; // Saltamos los siguientes tres índices, ya que ya fueron reemplazados
+    } else if (fraseModificada.slice(i, i + 4) == "ufat") {
+      textoOriginal += "u";
+      i += 3; // Saltamos los siguientes tres índices, ya que ya fueron reemplazados
+    } else {
+      textoOriginal += fraseModificada[i];
     }
-    return textoOriginal;
   }
+  return textoOriginal;
+}
 
 
-  
 
-  
-  
-  
+
+
+
+
